34f4cd965a04aec8db09e99c05efcfbbfc18a13b
SakshitAllumolu <115934476+SakshitAllumolu@users.noreply.github.com>
2023-08-08 22:35:15 +0530

    Add files via upload

---
+++ recept-dashboard.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+<title>Preskool - Basic Inputs</title>
+
+<link rel="shortcut icon" href="assets/img/favicon.png">
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="assets/plugins/feather/feather.css">
+
+<link rel="stylesheet" href="assets/plugins/icons/flags/flags.css">
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+<body>
+<div class="main-wrapper">
+<?php
+include "recept-sidebar.php";
+?>
+<!-- <link rel="stylesheet" href="assets/plugins/simple-calendar/simple-calendar.css"> -->
+
+<div class="page-wrapper">
+<div class="content container-fluid">
+
+<div class="page-header">
+<div class="row">
+<div class="col-sm-12">
+<div class="page-sub-header">
+<h3 class="page-title">Welcome !</h3>
+<ul class="breadcrumb">
+<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+<li class="breadcrumb-item active">Manager</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="row">
+<div class="col-xl-3 col-sm-6 col-12 d-flex">
+<div class="card bg-comman w-100">
+<div class="card-body">
+<div class="db-widgets d-flex justify-content-between align-items-center">
+<div class="db-info">
+<h6>Total Customers</h6>
+<h3>240</h3>
+</div>
+<div class="db-icon">
+<img src="assets/img/icons/customers_adobe_express.svg" alt="Dashboard Icon">
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-3 col-sm-6 col-12 d-flex">
+<div class="card bg-comman w-100">
+<div class="card-body">
+<div class="db-widgets d-flex justify-content-between align-items-center">
+<div class="db-info">
+<h6>Total Suppliers</h6>
+<h3>100</h3>
+</div>
+<div class="db-icon">
+<img src="assets/img/icons/supplier_adobe_express.svg" alt="Dashboard Icon">
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-3 col-sm-6 col-12 d-flex">
+<div class="card bg-comman w-100">
+<div class="card-body">
+<div class="db-widgets d-flex justify-content-between align-items-center">
+<div class="db-info">
+<h6>Total Medicines Available</h6>
+<h3>100</h3>
+</div>
+<div class="db-icon">
+<img src="assets/img/icons/medicine_adobe_express.svg" alt="Dashboard Icon">
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-xl-3 col-sm-6 col-12 d-flex">
+<div class="card bg-comman w-100">
+<div class="card-body">
+<div class="db-widgets d-flex justify-content-between align-items-center">
+<div class="db-info">
+<h6>Invoices Generated</h6>
+<h3>40</h3>
+</div>
+<div class="db-icon">
+<img src="assets/img/icons/invoices_adobe_express.svg" alt="Dashboard Icon">
+</div>
+</div>
+</div>
+</div>
+</div> 
+</div>
+
+
+<div class="row">
+<div class="col-xl-3 col-sm-6 col-12 d-flex">
+<div class="card bg-comman w-100">
+<div class="card-body">
+<div class="db-widgets d-flex justify-content-between align-items-center">
+<div class="db-info">
+<h6>Out Of stock</h6>
+<h3>240</h3>
+</div>
+<div class="db-icon">
+<img src="assets/img/icons/outofstocks_adobe_express.svg" alt="Dashboard Icon">
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-3 col-sm-6 col-12 d-flex">
+<div class="card bg-comman w-100">
+<div class="card-body">
+<div class="db-widgets d-flex justify-content-between align-items-center">
+<div class="db-info">
+<h6>Expired</h6>
+<h3>100</h3>
+</div>
+<div class="db-icon">
+<img src="assets/img/icons/expired_adobe_express.svg" alt="Dashboard Icon">
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+<div class="row">
+<div class="col-xl-3 col-sm-6 col-12 d-flex">
+<div class="card bg-comman w-100">
+<div class="card-body">
+<div class="db-widgets d-flex justify-content-between align-items-center">
+<div class="db-info">
+<h6>Today Report</h6>
+<h3>240</h3>
+</div>
+<div class="db-icon">
+<img src="assets/img/icons/teacher-icon-01.svg" alt="Dashboard Icon">
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+<div class="row">                       
+        <div class="col-sm-6">
+            <div class="card widget-flat">
+                <div class="card-body">
+                    <div class="float-end">
+                        <!-- <i class="mdi mdi-cart-plus widget-icon"></i> -->
+                            </div>
+                        <a href="" data-bs-toggle="modal" data-bs-target="#signup-modal" ><h1 style="text-align:center"><i style="font-size: 60px" class="mdi mdi-plus widget-ico"></i></h1></a>
+                            <p class="mb-0 text-muted" style="text-align:center">Add Medicine
+                                <!-- <span class="text-danger me-2"><i class="mdi mdi-arrow-down-bold">Add Course</i> 1.08%</span> -->
+                                <!-- <span class="text-nowrap">Since last month</span> -->
+                            </p>
+                        </div> <!-- end card-body-->
+                </div> <!-- end card-->
+        </div>
+
+
+        <div class="col-sm-6">
+            <div class="card widget-flat">
+                <div class="card-body">
+                    <div class="float-end">
+                        <!-- <i class="mdi mdi-cart-plus widget-icon"></i> -->
+                            </div>
+                        <a href="" data-bs-toggle="modal" data-bs-target="#signup-modal" ><h1 style="text-align:center"><i style="font-size: 60px" class="mdi mdi-plus widget-ico"></i></h1></a>
+                            <p class="mb-0 text-muted" style="text-align:center">Add Supplier
+                                <!-- <span class="text-danger me-2"><i class="mdi mdi-arrow-down-bold">Add Course</i> 1.08%</span> -->
+                                <!-- <span class="text-nowrap">Since last month</span> -->
+                            </p>
+                        </div> <!-- end card-body-->
+                </div> <!-- end card-->
+        </div>
+</div>
+
+</body>
+
+
+
+
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/js/feather.min.js"></script>
+
+<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<script src="assets/js/script.js"></script>
+<script src="assets/plugins/apexchart/apexcharts.min.js"></script>
+<script src="assets/plugins/apexchart/chart-data.js"></script>
+
+<script src="assets/plugins/simple-calendar/jquery.simple-calendar.js"></script>
+<script src="assets/js/calander.js"></script>
+
+<script src="assets/js/circle-progress.min.js"></script>
+
---
+++ recept-online-appointments.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+<title>Preskool - Basic Inputs</title>
+
+<link rel="shortcut icon" href="assets/img/favicon.png">
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="assets/plugins/feather/feather.css">
+
+<link rel="stylesheet" href="assets/plugins/icons/flags/flags.css">
+
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+<div class="main-wrapper">
+<?php
+include "recept-sidebar.php";
+?>
+<div class="page-wrapper">
+<div class="content container-fluid">
+
+<div class="page-header">
+<div class="row align-items-center">
+<div class="col">
+<h3 class="page-title">Salary</h3>
+<ul class="breadcrumb">
+<li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+<li class="breadcrumb-item active">Salary</li>
+</ul>
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-12">
+<div class="card card-table">
+<div class="card-body">
+
+<div class="page-header">
+<div class="row align-items-center">
+<div class="col">
+<h3 class="page-title">Salary</h3>
+</div>
+<div class="col-auto text-end float-end ms-auto download-grp">
+<a href="#" class="btn btn-outline-primary me-2"><i class="fas fa-download"></i> Download</a>
+<a href="add-salary.html" class="btn btn-primary"><i class="fas fa-plus"></i></a>
+</div>
+</div>
+</div>
+
+<div class="table-responsive">
+<table class="table border-0 star-student table-hover table-center mb-0 datatable table-striped">
+<thead class="student-thread">
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Gender</th>
+<th>Joining Date</th>
+<th>Amount</th>
+<th class="text-end">Status</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>PRE2209</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-01.jpg" alt="User Image"></a>
+<a>Aaliyah</a>
+</h2>
+</td>
+<td>Female</td>
+<td>17 Aug 2020</td>
+<td>$320</td>
+<td class="text-end">
+<span class="badge badge-success">Paid</span>
+</td>
+</tr>
+<tr>
+<td>PRE2213</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-02.jpg" alt="User Image"></a>
+<a>Malynne</a>
+</h2>
+</td>
+<td>Female</td>
+<td>05 Aug 2020</td>
+<td>$536</td>
+<td class="text-end">
+<span class="badge badge-success">Paid</span>
+</td>
+</tr>
+<tr>
+<td>PRE2143</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-03.jpg" alt="User Image"></a>
+<a>Levell Scott</a>
+</h2>
+</td>
+<td>Male</td>
+<td>04 Sept 2020</td>
+<td>$378</td>
+<td class="text-end">
+<span class="badge badge-success">Paid</span>
+</td>
+</tr>
+<tr>
+<td>PRE2431</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-04.jpg" alt="User Image"></a>
+<a>Minnie</a>
+</h2>
+</td>
+<td>Female</td>
+<td>17 Sept 2020</td>
+<td>$246</td>
+<td class="text-end">
+<span class="badge badge-danger">Unpaid</span>
+</td>
+</tr>
+<tr>
+<td>PRE1534</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-05.jpg" alt="User Image"></a>
+<a>Lois A</a>
+</h2>
+</td>
+<td>Male</td>
+<td>02 Oct 2020</td>
+<td>$560</td>
+<td class="text-end">
+<span class="badge badge-danger">Unpaid</span>
+</td>
+</tr>
+<tr>
+<td>PRE2153</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-06.jpg" alt="User Image"></a>
+<a>Calvin</a>
+</h2>
+</td>
+<td>Male</td>
+<td>28 Oct 2020</td>
+<td>$236</td>
+<td class="text-end">
+<span class="badge badge-danger">Unpaid</span>
+</td>
+</tr>
+<tr>
+<td>PRE1252</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-07.jpg" alt="User Image"></a>
+<a>Joe Kelley</a>
+</h2>
+</td>
+<td>Female</td>
+<td>17 Oct 2020</td>
+<td>$237</td>
+<td class="text-end">
+<span class="badge badge-success">Paid</span>
+</td>
+</tr>
+<tr>
+<td>PRE1434</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-08.jpg" alt="User Image"></a>
+<a>Vincent</a>
+</h2>
+</td>
+<td>Male</td>
+<td>05 Nov 2020</td>
+<td>$567</td>
+<td class="text-end">
+<span class="badge badge-success">Paid</span>
+</td>
+</tr>
+<tr>
+<td>PRE2345</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-09.jpg" alt="User Image"></a>
+<a>Kozma  Tatari</a>
+</h2>
+</td>
+<td>Female</td>
+<td>12 Nov 2020</td>
+<td>$564</td>
+<td class="text-end">
+<span class="badge badge-success">Paid</span>
+</td>
+</tr>
+<tr>
+<td>PRE2365</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-10.jpg" alt="User Image"></a>
+<a>John Chambers</a>
+</h2>
+</td>
+<td>Male</td>
+<td>15 Nov 2020</td>
+<td>$234</td>
+<td class="text-end">
+<span class="badge badge-success">Paid</span>
+</td>
+</tr>
+<tr>
+<td>PRE1234</td>
+<td>
+<h2 class="table-avatar">
+<a class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-11.jpg" alt="User Image"></a>
+<a>Nathan Humphries</a>
+</h2>
+</td>
+<td>Male</td>
+<td>17 Nov 2020</td>
+<td>$278</td>
+<td class="text-end">
+<span class="badge badge-success">Paid</span>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/js/feather.min.js"></script>
+
+<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<script src="assets/js/script.js"></script>
+<script src="assets/plugins/apexchart/apexcharts.min.js"></script>
+<script src="assets/plugins/apexchart/chart-data.js"></script>
+
+<script src="assets/plugins/simple-calendar/jquery.simple-calendar.js"></script>
+<script src="assets/js/calander.js"></script>
+
+<script src="assets/js/circle-progress.min.js"></script>
